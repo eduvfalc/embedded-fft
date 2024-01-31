676e4fe54bcee32abd93f62ebd66a5b9a4ca9eaa
Eduardo Vieira <eduardo.vieira.falcao@gmail.com>
2024-01-31 17:20:38 +0100

    Apply code formatting

--- src/SignalGenerator.cpp
+++ src/SignalGenerator.cpp
@@ -1,7 +1,7 @@
 #include "SignalGenerator.hpp"
 #include <math.h>
-#include <vector>
 #include <numbers>
+#include <vector>
 
 void
 SignalGenerator::GenerateSine(std::vector<std::pair<double, double>> parameters)
@@ -9,7 +9,8 @@ SignalGenerator::GenerateSine(std::vector<std::pair<double, double>> parameters)
     for (std::chrono::nanoseconds dt(0); dt <= mDuration; dt += mSamplingPeriod) {
         double signal_value = 0;
         for (const auto& sine : parameters) {
-            signal_value += std::sin(sine.first * std::numbers::pi* sine.second * std::chrono::duration<double>(dt).count());
+            signal_value
+                += std::sin(sine.first * std::numbers::pi * sine.second * std::chrono::duration<double>(dt).count());
         }
         Complex sample = {signal_value, 0};
         mSignal.emplace_back(sample);
