786059a820f55d153c667745bfff5ec62ad25ca3
Eduardo Vieira <eduardo.vieira.falcao@gmail.com>
2024-02-02 16:53:11 +0100

    Refactor test case, add CalculateError()

--- tests/TestFFT.cpp
+++ tests/TestFFT.cpp
@@ -8,6 +8,8 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
+constexpr auto kAmplitudeTolerance = 0.25;
+constexpr auto kFrequencyTolerance = 0.05;
 
 using Complex = std::complex<double>;
 
@@ -18,6 +20,8 @@ protected:
     std::shared_ptr<IFFT>      fft       = std::make_shared<FFT>(fft_utils);
     std::pair<double, double>
     FindPeak(std::vector<Complex>& signal, const std::chrono::nanoseconds& sampling_period);
+    std::pair<double, double>
+    CalculateError(const std::pair<double, double>& peak_data, const std::pair<double, double>& parameters);
 };
 
 std::pair<double, double>
@@ -37,21 +41,27 @@ TestFFT::FindPeak(std::vector<Complex>& signal, const std::chrono::nanoseconds& 
     return peakData;
 }
 
+std::pair<double, double>
+TestFFT::CalculateError(const std::pair<double, double>& peak_data, const std::pair<double, double>& parameters)
+{
+    return std::pair<double, double>{1 - peak_data.first / parameters.first, 1 - peak_data.second / parameters.second};
+}
+
 TEST_F(TestFFT, ComputeSineWaveSpectrum)
 {
     std::vector<Complex>             test_signal;
-    std::pair<double, double>        parameters{5, 60};
+    const std::pair<double, double>  parameters{5, 60};
     const std::chrono::nanoseconds   sampling_period = std::chrono::milliseconds(1);
     const std::chrono::nanoseconds   duration        = std::chrono::seconds(2);
-    std::shared_ptr<SignalGenerator> sig_gen         = std::make_shared<SignalGenerator>(duration, sampling_period);
+    std::shared_ptr<SignalGenerator> sig_gen
+        = std::make_shared<SignalGenerator>(std::chrono::seconds(2), std::chrono::milliseconds(1));
     sig_gen->GenerateSine(test_signal, parameters);
     sig_gen->ApplyHannWindow(test_signal);
 
     fft->Compute(test_signal);
-    auto         peak_data       = FindPeak(test_signal, sampling_period);
-    const double amplitude_error = 1 - peak_data.first / parameters.first;
-    const double frequency_error = 1 - peak_data.second / parameters.second;
+    const auto peak_data  = FindPeak(test_signal, sampling_period);
+    const auto peak_error = CalculateError(peak_data, parameters);
 
-    EXPECT_LE(amplitude_error, 0.25);
-    EXPECT_LE(frequency_error, 0.05);
+    EXPECT_LE(peak_error.first, kAmplitudeTolerance);
+    EXPECT_LE(peak_error.second, kFrequencyTolerance);
 }
