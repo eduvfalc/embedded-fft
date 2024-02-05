6553aa4a2fa3b75d5c0a68b40ed1ab485169a756
Eduardo Vieira <eduardo.vieira.falcao@gmail.com>
2024-02-05 13:21:33 +0100

    Add the maximum percentual frequency delta as a member variable

--- include/DSPUtils.hpp
+++ include/DSPUtils.hpp
@@ -12,6 +12,9 @@ class DSPUtils : public IDSPUtils
 public:
     DSPUtils() = default;
 
+    explicit DSPUtils(double max_percent_freq_delta)
+      : mMaxPercentFrequencyDelta(max_percent_freq_delta){};
+
     void
     BitReversal(std::vector<Complex>& signal) override;
 
@@ -20,6 +23,9 @@ public:
 
     std::vector<std::pair<double, double>>
     FindPeaks(std::vector<Complex>& signal, std::chrono::nanoseconds sampling_period, int max_peaks) override;
+
+private:
+    double mMaxPercentFrequencyDelta = 0.05;
 };
 
 #endif  // H_DSP_UTILS_HPP
--- src/DSPUtils.cpp
+++ src/DSPUtils.cpp
@@ -5,7 +5,6 @@
 #include <vector>
 
 using Complex = std::complex<double>;
-
 void
 DSPUtils::BitReversal(std::vector<Complex>& signal)
 {
@@ -43,7 +42,6 @@ std::vector<std::pair<double, double>>
 DSPUtils::FindPeaks(std::vector<Complex>& signal, std::chrono::nanoseconds sampling_period, int max_peaks)
 {
     std::vector<std::pair<double, double>> peak_data(max_peaks);
-    const auto                             tolerance = 0.05;
     const auto t_s         = std::chrono::duration_cast<std::chrono::duration<double>>(sampling_period).count();
     const int  signal_size = signal.size();
     for (int i = 1; i <= signal_size / 2; ++i) {
@@ -53,10 +51,9 @@ DSPUtils::FindPeaks(std::vector<Complex>& signal, std::chrono::nanoseconds sampl
         });
         if (min_it != peak_data.end() && amplitude > min_it->first) {
             const auto frequency = i * 1 / (signal_size * t_s);
-            auto       neighbor_it
-                = std::find_if(peak_data.begin(), peak_data.end(), [&frequency, &tolerance](const auto& peak) {
-                      return std::abs(1 - frequency / peak.second) < tolerance;
-                  });
+            auto neighbor_it = std::find_if(peak_data.begin(), peak_data.end(), [&frequency, this](const auto& peak) {
+                return std::abs(1 - frequency / peak.second) < mMaxPercentFrequencyDelta;
+            });
             if (neighbor_it != peak_data.end()) {
                 if (amplitude > neighbor_it->first) {
                     *neighbor_it = {amplitude, frequency};
