c75fd812a7d8154297ee570bee668fa5128d5e9f
Eduardo Vieira Falcão <eduardo.vieira.falcao@gmail.com>
2024-02-19 12:48:10 +0100

    Revert change

--- include/fft_types.hpp
+++ include/fft_types.hpp
@@ -6,7 +6,10 @@
 
 namespace fftemb
 {
-using cnl_type = cnl::fixed_point<cnl::rounding_integer<int32_t, cnl::_impl::tag_t<cnl::rounding_integer<>>>, -20>;
+// from github.com/hbe72/cdsp
+using cnl_type = cnl::fixed_point<
+    cnl::rounding_integer<cnl::elastic_integer<24, int32_t>, cnl::_impl::tag_t<cnl::rounding_integer<>>>,
+    -20>;
 
 using Complex = std::complex<cnl_type>;
 }  // namespace fftemb
