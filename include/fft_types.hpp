bd03103ba8a572628bc58de219225e9c62ab3ac2
Eduardo Vieira Falcão <eduardo.vieira.falcao@gmail.com>
2024-02-21 14:51:32 +0100

    Update type name

--- include/fft_types.hpp
+++ include/fft_types.hpp
@@ -13,12 +13,12 @@
 namespace fftemb
 {
 /// @brief The type used to represent the values of the FFT calculations
-using safe_rounding_integer
+using safe_rounding_elastic_integer
     = cnl::fixed_point<cnl::overflow_integer<cnl::rounding_integer<cnl::elastic_integer<31, int32_t>,
                                                                    cnl::_impl::tag_t<cnl::rounding_integer<>>>>,
                        -20>;
 /// @brief The complex type
-using Complex = std::complex<safe_rounding_integer>;
+using Complex = std::complex<safe_rounding_elastic_integer>;
 }  // namespace fftemb
 
 #endif  // H_FFT_TYPES_HPP
